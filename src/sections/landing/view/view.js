5264942be621f2ffe47965ebec23621efed10403
hesamgh <hesamghasemi86@gmail.com>
2025-02-17 15:18:24 +0330

    delete

--- src/sections/landing/view/view.js
+++ src/sections/landing/view/view.js
@@ -1,74 +1,72 @@
 "use client";
 
 // @mui
-import { useState, useCallback } from "react";
+// import { useState, useCallback } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { TextField } from "@mui/material";
+// import { TextField } from "@mui/material";
 
 // components
 import { useSettingsContext } from "@/src/components/settings";
-import { TronWeb } from "tronweb";
-import LoadingButton from "@mui/lab/LoadingButton";
+// import { TronWeb } from "tronweb";
+// import LoadingButton from "@mui/lab/LoadingButton";
 
 export default function DashboardView() {
   const settings = useSettingsContext();
 
-  const [wallet, setWallet] = useState({
-    address: "",
-    privateKey: "",
-  });
+  // const [wallet, setWallet] = useState({
+  //   address: "",
+  //   privateKey: "",
+  // });
 
-  const [prefix, setPrefix] = useState("");
-  const [suffix, setSuffix] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [prefix, setPrefix] = useState("");
+  // const [suffix, setSuffix] = useState("");
+  // const [loading, setLoading] = useState(false);
 
-  const tronWeb = new TronWeb({
-    fullHost: "https://api.trongrid.io",
-  });
+  // const tronWeb = new TronWeb({
+  //   fullHost: "https://api.trongrid.io",
+  // });
 
-  const generateVanityAddress = useCallback(async () => {
-    setLoading(true);
+  // const generateVanityAddress = useCallback(async () => {
+  //   setLoading(true);
 
-    let found = false;
-    let newWallet = {};
+  //   let found = false;
+  //   let newWallet = {};
 
-    while (!found) {
-      const newAccount = await tronWeb.createAccount();
-      const generatedAddress = newAccount.address.base58;
+  //   while (!found) {
+  //     const newAccount = await tronWeb.createAccount();
+  //     const generatedAddress = newAccount.address.base58;
 
-      if (
-        generatedAddress.startsWith(prefix) &&
-        generatedAddress.endsWith(suffix)
-      ) {
-        found = true;
-        newWallet = {
-          address: generatedAddress,
-          privateKey: newAccount.privateKey,
-        };
+  //     if (
+  //       generatedAddress.startsWith(prefix) &&
+  //       generatedAddress.endsWith(suffix)
+  //     ) {
+  //       found = true;
+  //       newWallet = {
+  //         address: generatedAddress,
+  //         privateKey: newAccount.privateKey,
+  //       };
 
-        console.log("✅ کیف پول مشابه پیدا شد!");
-        console.log("آدرس: ", newWallet.address);
-        console.log("کلید خصوصی: ", newWallet.privateKey);
+  //       console.log("✅ کیف پول مشابه پیدا شد!");
+  //       console.log("آدرس: ", newWallet.address);
+  //       console.log("کلید خصوصی: ", newWallet.privateKey);
 
-        setWallet(newWallet);
-        setLoading(false);
-      }
-    }
+  //       setWallet(newWallet);
+  //       setLoading(false);
+  //     }
+  //   }
 
-    return newWallet;
-  }, [prefix, suffix]);
-
-  console.log("fff", loading);
+  //   return newWallet;
+  // }, [prefix, suffix]);
 
   return (
     <Container maxWidth={settings.themeStretch ? false : "lg"}>
       <Box textAlign="center" sx={{ pt: 10 }}>
         <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
-          کیف پول ترونی شما علی جان
+          salam
         </Typography>
-        <Box sx={{ mb: 4, maxWidth: 500, mx: "auto" }}>
+        {/* <Box sx={{ mb: 4, maxWidth: 500, mx: "auto" }}>
           <TextField
             label="ابتدای آدرس (Prefix)"
             variant="outlined"
@@ -127,7 +125,7 @@ export default function DashboardView() {
               کلید خصوصی: {wallet.privateKey}
             </Typography>
           </>
-        )}
+        )} */}
       </Box>
     </Container>
   );
